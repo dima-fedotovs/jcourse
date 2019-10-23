f77f15bfb43828ea15dd253e227b6ed954c683a5
appledg <denis.gubarev@office.ctco.lv>
2019-10-23 19:18:46 +0300

    Hello World

---
+++ lesson 1/HelloWorld.java
@@ -0,0 +1,12 @@
+/* 
+  This is my first program on Java
+*/
+
+class HelloWorld {
+
+    // Program begins with a call to main()
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
