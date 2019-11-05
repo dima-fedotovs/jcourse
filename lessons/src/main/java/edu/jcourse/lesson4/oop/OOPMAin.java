4a322e58d41e48ef1ca7831c9195601f8534c0b7
appledg <denis.gubarev@gmail.com>
2019-11-05 10:42:57 +0200

    Lesson 4

--- lessons/src/main/java/edu/jcourse/lesson4/oop/OOPMAin.java
+++ lessons/src/main/java/edu/jcourse/lesson4/oop/OOPMAin.java
@@ -3,12 +3,12 @@ package edu.jcourse.lesson4.oop;
 public class OOPMAin {
 
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.name = "Billy";
-        dog.sayHello();
-
-        Cat cat = new Cat();
-        cat.name = "Kitty";
-        cat.sayHello();
+//        Dog dog = new Dog();
+//        dog.name = "Billy";
+//        dog.sayHello();
+//
+//        Cat cat = new Cat();
+//        cat.name = "Kitty";
+//        cat.sayHello();
     }
 }
