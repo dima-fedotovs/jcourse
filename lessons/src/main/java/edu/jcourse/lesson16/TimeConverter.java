83dbed5b1faed0f0c476e5360c03a824a0af3ad3
Dimitrijs Fedotovs <dima@fedoto.ws>
2020-01-06 18:16:28 +0200

    2020-01-06 - start time converter (tasks)

---
+++ lessons/src/main/java/edu/jcourse/lesson16/TimeConverter.java
@@ -0,0 +1,17 @@
+package edu.jcourse.lesson16;
+
+import java.util.Scanner;
+
+public class TimeConverter {
+
+    public static void main(String[] args) {
+        // Спросить у пользователя:
+        // 1. Timezone пункта отправления (String)
+        // 2. Дата и время отправления (LocalDateTime)
+        // 3. Timezone пункта назначения (String)
+        // 4. Дата и время прибытия (LocalDateTime)
+
+
+
+    }
+}
